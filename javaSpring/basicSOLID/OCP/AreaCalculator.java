e0a4fd9954fa3bb2bb92ffddf9c50aeb8aad22fb
ejdrp1 <ejdrp1@naver.com>
2023-12-06 14:57:01 +0900

    feat(OCP) : 클래스 생성 및 분리

---
+++ javaSpring/basicSOLID/OCP/AreaCalculator.java
@@ -0,0 +1,12 @@
+package OCP;
+
+public class AreaCalculator {
+    public double calculateArea(Shape[] shapes) {
+        double totalArea = 0;
+        for (Shape shape : shapes) {
+            totalArea += shape.calculateArea();
+        }
+        return totalArea;
+    }
+}
+
