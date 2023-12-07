3307c90d9f889567ac757065238a3f024d405a08
ejdrp1 <ejdrp1@naver.com>
2023-12-07 20:49:29 +0900

    feat(Shape) : 클래스 생성 및 수정

--- javaSpring/basicSOLID/isp/Cube.java
+++ javaSpring/basicSOLID/isp/Cube.java
@@ -1,4 +1,15 @@
 package isp;
 
-public class Cube {
+public class Cube implements Shape {
+    private double width;
+    private double height;
+    private double depth;
+
+    public double calculateArea() {
+        return 2 * (width * height + width * depth + height * depth);
+    }
+
+    public double calculateVolume() {
+        return width * height * depth;
+    }
 }
--- javaSpring/basicSOLID/isp/RectAngle.java
+++ javaSpring/basicSOLID/isp/RectAngle.java
@@ -1,4 +1,14 @@
 package isp;
 
-public class RectAngle {
+public class RectAngle implements Shape{
+    private double width;
+    private double height;
+
+    public double calculateArea() {
+        return width * height;
+    }
+
+    public double calculateVolume() {
+        throw new UnsupportedOperationException();
+    }
 }
